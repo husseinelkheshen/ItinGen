3b0247e748f5c2a85a42c7d67244f50682b65db3
avsurganov <avsurganov@cs.uchicago.edu>
2018-11-07 13:42:10 -0600

    finished all tests for iteraton 1

--- test/unit/specs/ItinGen.spec.js
+++ test/unit/specs/ItinGen.spec.js
@@ -10,14 +10,54 @@ describe('ItinGen.vue', () => {
       .toEqual('Itinerary Generator')
   })
 
+  it('should render the map', () => {
+    expect(vm.$el.querySelector('.app div'))
+      .contains('.vue-map-container')
+  })
+
+  it('should display a location', () => {
+    expect(vm.$el.querySelector('.app div'))
+      .contains('.vue-map-marker')
+  })
+
+  it('should display a sidebar', () => {
+    expect(vm.$el.querySelector('.app div'))
+      .contains('.sidebar')
+  })
+
+  it('should display a login button', () => {
+    expect(vm.$el.querySelector('.sidebar button').textContent)
+      .toEqual('Login')
+  })
+
+  it('should display a sign up button', () => {
+    expect(vm.$el.querySelector('sidebar button').textContent)
+      .toEqual('Signup')
+  })
+
+  it('should display an itinerary', () => {
+    expect(vm.$el.querySelector('sidebar div'))
+      .containes('.itinerary')
+  })
+
   it('should render the generate button', () => {
     expect(vm.$el.querySelector('.generate button').textContent)
       .toEqual('Generate')
   })
 
-  it('should render the map', () => {
-    expect(vm.$el.querySelector('.app div'))
-      .contains('.vue-map-container')
+  it('should render the dislike button', () => {
+    expect(vm.$el.querySelector('.dislike button').textContent)
+      .toEqual('Dislike')
+  })
+
+  it('should render a save current itinerary button', () => {
+    expect(vm.$el.querySelector('.save button').textContent)
+      .toEqual('Save Current Itinerary')
+  })
+
+  it('should render a link to saved itineraries', () => {
+    expect(vm.$el.querySelector('.saved button').textContent)
+      .toEqual('View Saved Itineraries')
   })
 
 })
