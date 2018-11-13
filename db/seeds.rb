46caad7b9d5c4350f9505662ae1542239d5c9e23
Omar Salemohamed <omar.salemohamed@gmail.com>
2018-11-13 00:29:23 -0600

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require "json"
-venues = JSON.parse(File.read('activities_data/venues_11122018.json'))
-venues.each do |venue|
-	Venue.create!(venue)
+
+Dir.glob('activities_data/events/*.json').each do |events_json|	
+	events = JSON.parse(File.read(events_json))
+	events.each do |event|
+		Event.create!(event)
+end
+
+Dir.glob('activities_data/venues/*.json').each do |venues_json|	
+	venues = JSON.parse(File.read(venues_json))
+	venues.each do |venue|
+		Venue.create!(venue)
 end
