33f06720ad5a90ed05326306931beb02111acaaf
Omar Salemohamed <omar.salemohamed@gmail.com>
2018-11-12 23:59:56 -0600

    add event/venue schema

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,23 @@
 class Event
   include Mongoid::Document
+  field :venue_id, type: String
+  field :event_name, type: String
+  field :event_alias, type: String
+  field :event_id, type: String
+  field :mon_start, type: Integer
+  field :mon_end, type: Integer
+  field :tues_start, type: Integer
+  field :tues_end, type: Integer
+  field :wed_start, type: Integer
+  field :wed_end, type: Integer
+  field :thurs_start, type: Integer
+  field :thurs_end, type: Integer
+  field :fri_start, type: Integer
+  field :fri_end, type: Integer
+  field :sat_start, type: Integer
+  field :sat_end, type: Integer
+  field :sun_start, type: Integer
+  field :sun_end, type: Integer
+  field :tags, type: Array
+  field :price, type: String
 end
