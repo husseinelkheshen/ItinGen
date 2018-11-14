d196feeed83bfe711e9385680b70eb18683797cd
Anthony Surganov <avsurganov@gmail.com>
2018-11-13 19:54:18 -0600

    linted migration files

--- db/migrate/20181114012748_create_tevents.rb
+++ db/migrate/20181114012748_create_tevents.rb
@@ -1,14 +1,14 @@
 class CreateTevents < ActiveRecord::Migration[5.2]
   def change
     create_table :tevents do |t|
-  	  t.string :venue_id
-  	  t.string :event_name
-  	  t.string :event_id
-  	  t.integer :start
-  	  t.integer :end
-  	  t.string :date
-  	  t.string :tags, array: true
-  	  t.integer :price
+      t.string :venue_id
+      t.string :event_name
+      t.string :event_id
+      t.integer :start
+      t.integer :end
+      t.string :date
+      t.string :tags, array: true
+      t.integer :price
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 require "json"
 
 Dir.glob('activities_data/venues/*.json').each do |venues_json|
-	venues = JSON.parse(File.read(venues_json))
-	venues.each do |venue|
-		Venue.create!(venue)
-	end
+  venues = JSON.parse(File.read(venues_json))
+  venues.each do |venue|
+    Venue.create!(venue)
+  end
 end
 
 Dir.glob('activities_data/tmp_events/*.json').each do |events_json|
-	tevents = JSON.parse(File.read(tevents_json))
-	tevents.each do |tevent|
-		Tevent.create!(tevent)
-	end
+  tevents = JSON.parse(File.read(tevents_json))
+  tevents.each do |tevent|
+    Tevent.create!(tevent)
+  end
 end
