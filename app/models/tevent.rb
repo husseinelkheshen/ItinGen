3bde3d1a809537f4f4e8de85d080167669f26953
Omar Salemohamed <omar.salemohamed@gmail.com>
2018-11-13 22:09:41 -0600

    added model validations

--- app/models/tevent.rb
+++ app/models/tevent.rb
@@ -1,2 +1,10 @@
 class Tevent < ApplicationRecord
+	validate :venue_id, presence: true
+	validate :event_name, presence: true
+	validate :event_id, presence: true
+	validate :start, presence: true
+	validate :end, presence: true
+	validate :date, presence: true
+	validate :tags, array: true, presence: true
+	validate :price, presence: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,13 @@
 class Venue < ApplicationRecord
+    validate :venue_id, presence: true
+    validate :venue_name, presence: true
+    validate :venue_alias, presence: true
+    validate :latitude, presence: true
+    validate :longitude, presence: true
+    validate :address1, presence: true
+    validate :address2, presence: true
+    validate :address3, presence: true
+    validate :city, presence: true
+    validate :state, presence: true
+    validate :zip_code, presence: true
 end
