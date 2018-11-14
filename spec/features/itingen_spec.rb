14266235c81408bbdead103cda985225b8113adf
Anthony Surganov <avsurganov@gmail.com>
2018-11-13 19:50:58 -0600

    rspec tests passing except for 3

--- spec/features/itingen_spec.rb
+++ spec/features/itingen_spec.rb
@@ -10,45 +10,46 @@ describe 'content' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'should have a title' do
-      page.should have_css('h1', :text => 'Itinerary Generator')
-    end
-
     describe 'maps' do
       it 'should display google maps' do
         page.should have_css('div', :id => 'map')
       end
 
+      # Checking to see if View propogated venues that map is using
       it 'should display a location' do
-        page.should have_css('div', :class => 'marker')
+        page.should have_css('h5', :class => 'card-title')
       end
     end
 
     describe 'sidebar' do
       describe 'menu' do
         it 'should have a menu' do
-          page.should have_css('ul', :text => 'Menu')
+          page.should have_css('div', :id => 'mySidebar')
         end
 
         it 'should have an option to login' do
-          page.should have_css('button', :text => 'Login')
+          page.should have_css('button', :text => 'Log in/out')
         end
 
         it 'should have an option to sign up' do
-          page.should have_css('button', :text => 'Sign Up')
+          page.should have_css('button', :text => 'Create Account')
         end
       end
 
       it 'should have an itinerary' do
-        page.should have_css('div', :class => 'itinerary')
+        page.should have_css('div', :class => 'card-header')
       end
 
       it 'should have a generate button' do
         page.should have_css('button', :id => 'Generate')
       end
 
+      it 'should have a like button' do
+        page.should have_css('a', :id => 'like')
+      end
+
       it 'should have a dislike button' do
-        page.should have_css('button', :text => 'Dislike')
+        page.should have_css('a', :id => 'dislike')
       end
 
       it 'should have a save current itinerary button' do
