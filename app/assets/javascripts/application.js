ab6f27953c1718914b731fd64f10361fdecbff42
Anthony Surganov <avsurganov@gmail.com>
2018-11-13 19:19:13 -0600

    redid app in postgresql

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require gmaps/google
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
