f2be9f644710d2a9e8fd54914d54485544114598
Omar Salemohamed <omar.salemohamed@gmail.com>
2018-11-13 19:54:56 -0600

    up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,16 @@ Dir.glob('activities_data/venues/*.json').each do |venues_json|
 	end
 end
 
-Dir.glob('activities_data/tmp_events/*.json').each do |events_json|
+Dir.glob('activities_data/tmp_events/*.json').each do |tevents_json|
 	tevents = JSON.parse(File.read(tevents_json))
 	tevents.each do |tevent|
 		Tevent.create!(tevent)
+	end
+end
+
+Dir.glob('activities_data/events/*.json').each do |pevents_json|
+	pevents = JSON.parse(File.read(pevents_json))
+	pevents.each do |pevent|
+		Pevent.create!(pevent)
 	end
 end
